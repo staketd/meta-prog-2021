3f463a1960e92f5fb6009a2fa7ab4065cdc4f23e
staketd <pushind00@yandex.ru>
2021-10-24 09:55:20 +0300

    fixing issues

--- task1/string_mapper.hpp
+++ task1/string_mapper.hpp
@@ -10,7 +10,7 @@ using namespace std::literals::string_view_literals;
 template<size_t max_length>
 class String {
 public:
-    constexpr String(const char* string, size_t length) : length_(std::min(length, max_length)) {
+    constexpr String(const char *string, size_t length) : length_(std::min(length, max_length)) {
         for (size_t i = 0; i < length; ++i) {
             string_[i] = string[i];
         }
@@ -24,7 +24,7 @@ public:
     char string_[max_length];
 };
 
-constexpr String<256> operator ""_cstr(const char* literal, size_t length) {
+constexpr String<256> operator ""_cstr(const char *literal, size_t length) {
     return {literal, length};
 }
 
@@ -36,47 +36,46 @@ static_assert(String<128>{"some text", 4} == "some"sv);
 template<class From, auto target>
 struct Mapping {
     using from = From;
-
-    static constexpr auto get() {
-        return target;
-    }
+    static constexpr auto MappingValue = target;
 };
 
 template<class M, class Target>
 concept IsMapping = requires(M m) {
-    ([]<class From, Target target> (Mapping<From, target> m) {})(m);
+    ([]<class From, Target target>(Mapping<From, target> m) {})(m);
 };
 
 template<class M, class Base, class Target>
-concept IsMappingAndConvertibleTo = IsMapping<M, Target> && std::is_convertible<typename M::from, Base>::value;
+concept IsMappingAndDerivedFrom = IsMapping<M, Target> && std::derived_from<typename M::from, Base>
+                                  && std::is_convertible_v<decltype(M::MappingValue), Target>;
 
 namespace inner {
 
-    template<size_t I, size_t N, class Base, class Target, IsMappingAndConvertibleTo<Base, Target>... Mappings>
+    template<class Base, class Target, IsMappingAndDerivedFrom<Base, Target>... Mappings>
     struct MapHelper {
         static std::optional<Target> doMap(const Base &object) {
-            try {
-                dynamic_cast<const typename std::tuple_element_t<I, std::tuple<Mappings...>>::from &>(object);
-                return ((typename std::tuple_element_t<I, std::tuple<Mappings...>>) {}).get();
-            } catch (std::bad_cast &e) {
-                return MapHelper<I + 1, N, Base, Target, Mappings...>::doMap(object);
-            }
+            return std::optional<Target> {};
         }
     };
 
-    template<size_t N, class Base, class Target, IsMappingAndConvertibleTo<Base, Target>... Mappings>
-    struct MapHelper<N, N, Base, Target, Mappings...> {
+    template<class Base, class Target, IsMappingAndDerivedFrom<Base, Target> HeadMapping,
+            IsMappingAndDerivedFrom<Base, Target>... Mappings>
+    struct MapHelper<Base, Target, HeadMapping, Mappings...> {
         static std::optional<Target> doMap(const Base &object) {
-            return std::optional<Target>{};
+            auto *cast = dynamic_cast<const typename HeadMapping::from *>(&object);
+            if (cast != nullptr) {
+                return HeadMapping::MappingValue;
+            } else if (sizeof...(Mappings) == 0) {
+                return std::optional<Target>{};
+            } else {
+                return MapHelper<Base, Target, Mappings...>::doMap(object);
+            }
         }
     };
 }
 
-template<class Base, class Target, IsMappingAndConvertibleTo<Base, Target>... Mappings>
+template<class Base, class Target, IsMappingAndDerivedFrom<Base, Target>... Mappings>
 struct ClassMapper {
-    template<int I>
-    using Helper = inner::MapHelper<I, sizeof...(Mappings), Base, Target, Mappings...>;
     static std::optional<Target> map(const Base &object) {
-        return Helper<0>::doMap(object);
+        return inner::MapHelper<Base, Target, Mappings...>::doMap(object);
     }
 };
